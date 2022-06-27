2a0b0d9da35f2938f13e9383fb63e20e88c67201
Joaquin Nader <joaquinnader@gmail.com>
2022-06-27 17:58:12 -0300

    added styled components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import './button.css';
 
 export interface IButton {
   label: string;
 }
 
+const StyledButton = styled.button`
+  color: blue;
+`
+
 const Button = ({ label }: IButton) => {
-  return <button>{label}</button>
+  return <StyledButton>{label}</StyledButton>
 }
 
 export default Button;
